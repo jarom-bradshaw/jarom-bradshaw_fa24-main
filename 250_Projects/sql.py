8acc8321bb3d7489eb5e224e88c140493c194bd0
jarom-bradshaw <156943555+jarom-bradshaw@users.noreply.github.com>
2024-11-06 23:28:50 -0700

    rough draft for P1

--- 250_Projects/sql.py
+++ 250_Projects/sql.py
@@ -5,19 +5,19 @@ import sqlite3
 
 # %%
 # careful to list your path to the file or save it in the same place as your .qmd or .py file
-sqlite_file = 'lahmansbaseballdb.sqlite'
-con = sqlite3.connect(sqlite_file)
+# sqlite_file = 'lahmansbaseballdb.sqlite'
+# con = sqlite3.connect(sqlite_file)
 
-q = 'SELECT * FROM allstarfull LIMIT 5'
-results = pd.read_sql_query(q,con)
+# q = 'SELECT * FROM allstarfull LIMIT 5'
+# results = pd.read_sql_query(q,con)
 
-results
+# results
 
-q = '''
-    SELECT * 
-    FROM sqlite_master 
-    WHERE type='table'
-    '''
-table = pd.read_sql_query(q,con)
-table.filter(['name'])
+# q = '''
+#     SELECT * 
+#     FROM sqlite_master 
+#     WHERE type='table'
+#     '''
+# table = pd.read_sql_query(q,con)
+# table.filter(['name'])
 # %%
